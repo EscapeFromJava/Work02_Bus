f0dd0bb6e25a9b27e9887219800eb1a81c11b093
EscapeFromJava <reske.artur@gmail.com>
2021-09-24 02:13:42 +0300

    Ver 1.0

---
+++ src/Bus.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Bus {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int i,n,h;
+        int sum;
+        System.out.print("Введите количество мостов: ");
+        n = in.nextInt();
+        for(i=1; i<=n; i++){
+            System.out.println("Введите высоту "+i+" моста: ");
+            h = in.nextInt();
+            sum =+h;
+            if (h<=437)  {
+                System.out.println("Crash " + i);
+                break;
+            }
+            if (i==n)
+                System.out.println("No crash");
+        }
+    }
+}
+
