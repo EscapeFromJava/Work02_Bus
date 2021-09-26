ac727e2c0abc83ed19239b1d9eb918b204db668f
EscapeFromJava <reske.artur@gmail.com>
2021-09-26 15:24:14 +0300

    Ver 1.2 (убрана переменная sum)

--- src/Bus.java
+++ src/Bus.java
@@ -3,13 +3,12 @@ import java.util.Scanner;
 public class Bus {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int i,n,h,sum;
+        int i,n,h;
         System.out.print("Введите количество мостов: ");
         n = in.nextInt();
         for(i=1; i<=n; i++){
             System.out.println("Введите высоту "+i+" моста: ");
             h = in.nextInt();
-            sum =+h;
             if (h<=437)  {
                 System.out.println("Crash " + i);
                 break;
